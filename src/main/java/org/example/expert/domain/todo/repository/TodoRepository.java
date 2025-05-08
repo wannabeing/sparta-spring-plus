1d75738d610fae755562d38add8c822e2f59bac2
wannabeing <wannabeing@naver.com>
2025-05-08 18:09:19 +0900

    refactor: [Lv2] QueryDSL 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryQueryDsl {
     @Query("SELECT t FROM Todo t "
         + "LEFT JOIN FETCH t.user u "
         + "WHERE (:weather is null OR t.weather LIKE :weather) "
@@ -23,9 +23,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
         @Param("start") LocalDateTime start,
         @Param("end") LocalDateTime end
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,33 @@
+package org.example.expert.domain.todo.repository;
+
+import java.util.Optional;
+
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoRepositoryImpl implements TodoRepositoryQueryDsl{
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public Optional<Todo> findByIdWithUser(Long todoId) {
+		QTodo todo = QTodo.todo;
+		QUser user = QUser.user;
+
+		Todo result = queryFactory
+			.selectFrom(todo)
+			.leftJoin(todo.user, user).fetchJoin()
+			.where(todo.id.eq(todoId))
+			.fetchOne();
+
+		return Optional.ofNullable(result);
+	}
+}
